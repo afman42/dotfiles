cf9f5b8a5a1e470240fc688565a0755936e7b8b5
Afif Arman <afifarman50@gmail.com>
2024-02-12 21:12:50 +0700

    add gopath, run home-manager. source bashrc

--- install-non-nixos.sh
+++ install-non-nixos.sh
@@ -13,18 +13,33 @@ echo "Successfully cli home-manager"
 
 # Add fnm eval
 echo "Add path fnm"
-echo -e "#fnm\n `eval fnm env --use-on-cd`" >> ~/.bashrc
+echo -e "#fnm\n `eval fnm env --use-on-cd` \n" >> ~/.bashrc
 echo "Successfully add path fnm"
+
+# Add path go
+echo "Add path GOPATH"
+echo -e "export PATH=$PATH:/home/arman/.nix-profile/bin/go \n" >> ~/.bashrc
+echo -e "export GOPATH=$HOME/go \n" >> ~/.bashrc
+echo -e "PATH=$GOPATH/bin:$PATH \n" >> ~/.bashrc
+echo "Successfully add GOPATH"
 
 # Add environment variable home-manager
 echo "Add path session variable"
-echo -e ". $HOME/.nix-profile/etc/profile.d/hm-session-vars.sh" >> ~/.profile
+echo -e "\n . $HOME/.nix-profile/etc/profile.d/hm-session-vars.sh \n" >> ~/.profile
 echo "Add path application dekstop"
-echo -e "export XDG_DATA_DIRS=$HOME/.nix-profile/share:$XDG_DATA_DIRS" >> ~/.profile
+echo -e "\n export XDG_DATA_DIRS=$HOME/.nix-profile/share:$XDG_DATA_DIRS \n" >> ~/.profile
 echo "Successfully add path session and application dekstop"
 
 # Clone folder
-echo "Execute clone"
+echo "Execute file clone shell script"
 chmod +x ./clone.sh
 sh ./clone.sh
 echo "Successfully clone folder"
+
+echo "Run home-manager"
+home-manager switch
+echo "Finish home-manager"
+
+echo "Source .bashrc"
+source ~/.bashrc
+echo "Finish Source .bashrc"
