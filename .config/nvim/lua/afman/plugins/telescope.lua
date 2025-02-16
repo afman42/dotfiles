e7679fbb099a72a6d9c1b7ac306a1dcda7706313
Afif Arman <afifarman50@gmail.com>
2025-02-16 15:25:32 +0700

    refactor: add telescope find file browser

--- .config/nvim/lua/afman/plugins/telescope.lua
+++ .config/nvim/lua/afman/plugins/telescope.lua
@@ -5,11 +5,12 @@ return {
     "nvim-lua/plenary.nvim",
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     "nvim-tree/nvim-web-devicons",
+    "nvim-telescope/telescope-file-browser.nvim",
   },
   config = function()
     local telescope = require("telescope")
     local actions = require("telescope.actions")
-
+    local fb_actions = require("telescope").extensions.file_browser.actions
     telescope.setup({
       defaults = {
         path_display = { "truncate " },
@@ -21,10 +22,46 @@ return {
           },
         },
       },
+      extensions = {
+        file_browser = {
+          theme = 'dropdown',
+          hijack_netrw = true,
+          mappings = {
+            ['i'] = {
+              ["<C-r>"] = fb_actions.rename,
+              ["<C-y>"] = fb_actions.copy,
+              ["<C-d>"] = fb_actions.remove,
+              ["<C-o>"] = fb_actions.open,
+              ["<C-n>"] = fb_actions.create,
+              ["<C-k>"] = actions.preview_scrolling_up,
+              ["<C-j>"] = actions.preview_scrolling_down,
+              ["<PageUp>"] = actions.move_selection_previous,
+              ["<PageDown>"] = actions.move_selection_next,
+            },
+            ['n'] = {
+              ["r"] = fb_actions.rename,
+              ["h"] = fb_actions.toggle_hidden,
+              ["a"] = fb_actions.toggle_all,
+              ["y"] = fb_actions.copy,
+              ["d"] = fb_actions.remove,
+              ["o"] = fb_actions.open,
+              ["N"] = fb_actions.create,
+              ["<bs>"] = fb_actions.backspace,
+              ["/"] = function()
+                vim.cmd("startinsert")
+              end,
+              ["<C-k>"] = actions.preview_scrolling_up,
+              ["<C-j>"] = actions.preview_scrolling_down,
+              ["<PageUp>"] = actions.move_selection_previous,
+              ["<PageDown>"] = actions.move_selection_next,
+            }
+          }
+        }
+      }
     })
 
     telescope.load_extension("fzf")
-
+    telescope.load_extension("file_browser")
     -- set keymaps
     local keymap = vim.keymap -- for conciseness
 
@@ -32,5 +69,9 @@ return {
     keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
     keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+
+    keymap.set("n", "<leader>fb", ":Telescope file_browser<CR>")
+    -- open file_browser with the path of the current buffer
+    keymap.set("n", "<leader>fb", ":Telescope file_browser path=%:p:h select_buffer=true<CR>")
   end,
 }
