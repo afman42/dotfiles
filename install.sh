21ebf0b5b45e0b1d260f1ebcf36e5b673bac84fe
Afif Arman <afifarman50@gmail.com>
2025-02-16 15:30:28 +0700

    refactor: remove double quote

--- install.sh
+++ install.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 cd ~;
-sudo apt install build-essential;
+'password' | sudo apt install build-essential -y;
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)";
+echo >> $HOME/.profile;
 echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> $HOME/.profile;
 echo >> $HOME/.bashrc;
 echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> $HOME/.bashrc;
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)";
 brew doctor;
-brew install curl unzip git gcc g++;
-brew install fnm neovim rust go posting bat;
+brew install curl unzip git gcc wget ripgrep fzf bat posting pipx fnm neovim rust go ;
 echo 'eval "$(fnm env --use-on-cd --shell bash)"' >> $HOME/.bashrc;
-echo '"export GOPATH=$HOME/go"' >> $HOME/.bashrc;
-echo 'export PATH="$PATH:$GOROOT/bin:$GOPATH/bin"' >> $HOME/.bashrc
-echo '"export PATH="$PATH:/usr/local/go/bin"' >> $HOME/.bashrc;
+echo 'export GOPATH=$HOME/go' >> $HOME/.bashrc;
+echo 'export PATH=$PATH:$GOROOT/bin:$GOPATH/bin' >> $HOME/.bashrc
+echo 'export PATH=$PATH:/usr/local/go/bin' >> $HOME/.bashrc;
 echo '. "$HOME/.cargo/env"' >> $HOME/.bashrc;
 echo '. "$HOME/.local/share/../bin/env"' >> $HOME/.bashrc;
 echo 'eval "$(fzf --bash)"' >> $HOME/.bashrc;
